71fa9e8b8f17c4325a0ff71c3de26b96d42daa4d
Helixoo <104375707+Helixoo@users.noreply.github.com>
2022-06-22 21:02:23 +0300

    Add files via upload

---
+++ cloner/install.bat
@@ -0,0 +1,18 @@
+@echo off
+pip install pypresence
+pip install asyncio
+pip install colorama
+pip install platform
+pip install discord
+pip install pyinstaller
+pip install pyperclip
+pip install colour
+pip install sys
+pip install pyarmor
+pip install psutil
+cls
+echo py main.py >> start.bat
+start.bat
+:pause
+
+"Satın Aldığınız İçin Teşekkürler!"
---
+++ cloner/start.bat
@@ -0,0 +1,4 @@
+:loop
+python main.py
+goto looppy main.py 
+py main.py 
